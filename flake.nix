b25990ea3f06f23f94876a79c1522d297b3891b9
Dawid Ciężarkiewicz <dpc@dpc.pw>
2023-09-23 14:16:22 -0700

    chore: switch to flakebox

--- flake.nix
+++ flake.nix
@@ -2,19 +2,15 @@
   description = "dpc's basic flake template";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    crane = {
-      url = "github:dpc/crane?rev=afdf9ab92ed2880c4e39aba5106677e3385717f4";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    fenix = {
-      url = "github:nix-community/fenix";
+    flakebox = {
+      url = "github:rustshop/flakebox?rev=36b349dc4e6802a0a26bafa4baef1f39fbf4e870";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, flake-utils, crane, fenix }:
+  outputs = { self, nixpkgs, flake-utils, flakebox }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
@@ -23,31 +19,8 @@
         lib = pkgs.lib;
         extLib = import ./nix/lib.nix { inherit lib; };
 
-        # `moreutils/bin/parallel` and `parallel/bin/parallel` conflict, so just use
-        # the binary we need from `moreutils`
-        moreutils-ts = pkgs.writeShellScriptBin "ts" "exec ${pkgs.moreutils}/bin/ts \"$@\"";
-
-        fenixChannel = fenix.packages.${system}.stable;
-        fenixChannelNightly = fenix.packages.${system}.latest;
-
-        fenixToolchain = (fenixChannel.withComponents [
-          "rustc"
-          "cargo"
-          "clippy"
-          "rust-analysis"
-          "rust-src"
-        ]);
-
-        fenixToolchainNightly = (fenixChannelNightly.withComponents [
-          "rustfmt"
-          "cargo"
-        ]);
-
-        fenixToolchainRustfmt = (fenixChannelNightly.withComponents [
-          "rustfmt"
-        ]);
-
-        craneLib = crane.lib.${system}.overrideToolchain fenixToolchain;
+        flakeboxLib = flakebox.lib.${system} { };
+        craneLib = flakeboxLib.craneLib;
 
         commonArgs =
           let
@@ -68,7 +41,6 @@
 
             installCargoArtifactsMode = "use-zstd";
 
-
             buildInputs = [ ];
 
             nativeBuildInputs = builtins.attrValues
@@ -78,59 +50,11 @@
           };
       in
       {
-        packages. default = craneLib.buildPackage ({ } // commonArgs);
+        packages.default = craneLib.buildPackage ({ } // commonArgs);
 
         devShells = {
-          default = pkgs.mkShell {
-
-            buildInputs = [ ] ++ commonArgs.buildInputs;
-
-            nativeBuildInputs = builtins.attrValues
-              {
-                inherit (pkgs) cargo-watch;
-                inherit fenixToolchain fenixToolchainRustfmt;
-                inherit (pkgs) nixpkgs-fmt shellcheck rnix-lsp just;
-                inherit (pkgs) lld parallel typos convco;
-              } ++ [
-              # This is required to prevent a mangled bash shell in nix develop
-              # see: https://discourse.nixos.org/t/interactive-bash-with-nix-develop-flake/15486
-              (pkgs.hiPrio pkgs.bashInteractive)
-              pkgs.nodePackages.bash-language-server
-
-            ] ++ commonArgs.nativeBuildInputs;
-
-            shellHook = ''
-              dot_git="$(git rev-parse --git-common-dir)"
-              if [[ ! -d "$dot_git/hooks" ]]; then mkdir "$dot_git/hooks"; fi
-              chmod +x .git/hooks/{pre-commit,commit-msg}
-              for hook in misc/git-hooks/* ; do ln -sf "$(pwd)/$hook" "$dot_git/hooks/" ; done
-              ${pkgs.git}/bin/git config commit.template $(pwd)/misc/git-hooks/commit-template.txt
-
-              # if running in direnv
-              if [ -n "''${DIRENV_IN_ENVRC:-}" ]; then
-                # and not set DIRENV_LOG_FORMAT
-                if [ -n "''${DIRENV_LOG_FORMAT:-}" ]; then
-                  >&2 echo "💡 Set 'DIRENV_LOG_FORMAT=\"\"' in your shell environment variables for a cleaner output of direnv"
-                fi
-              fi
-
-              >&2 echo "💡 Run 'just' for a list of available 'just ...' helper recipes"
-            '';
-          };
-
-          lint = pkgs.mkShell {
-            nativeBuildInputs = with pkgs; [
-              fenixToolchainNightly
-              nixpkgs-fmt
-              shellcheck
-              git
-              parallel
-              typos
-              moreutils-ts
-              nix
-            ] ++ lib.optionals (!pkgs.stdenv.isDarwin) [
-              semgrep
-            ];
+          default = flakeboxLib.mkDevShell {
+            packages = [ pkgs.mold ];
           };
         };
       }
--- justfile
+++ justfile
@@ -1,9 +1,14 @@
+alias b := build
+alias c := check
+alias t := test
+
+
 default:
   @just --list
 
 # run and restart on changes
 watch:
-  env RUST_LOG=${RUST_LOG:-debug},hyper=off cargo watch -x run
+  env RUST_LOG=${RUST_LOG:-debug} cargo watch -x run
   
 # run `cargo build` on everything
 build:
@@ -13,6 +18,25 @@ build:
 check:
   cargo check --workspace --all-targets
 
+# run tests
+test: build
+  cargo test
+
+# run lints (git pre-commit hook)
+lint:
+  env NO_STASH=true $(git rev-parse --git-common-dir)/hooks/pre-commit
+
+# run all checks recommended before opening a PR
+final-check: lint
+  cargo test --doc
+  just test
+
+# run code formatters
+format:
+  cargo fmt --all
+  nixpkgs-fmt $(echo **.nix)
+
+
 # run `cargo clippy` on everything
 clippy:
   cargo clippy --workspace --all-targets -- --deny warnings --allow deprecated
@@ -21,43 +45,29 @@ clippy:
 clippy-fix:
   cargo clippy --workspace --all-targets --fix
 
-# run tests
-test: build
-  cargo test
 
-# run lints (quick)
-lint:
-  env NO_STASH=true misc/git-hooks/pre-commit
-  just clippy
-  env RUSTDOCFLAGS='-D rustdoc::broken_intra_doc_links -D warnings' cargo doc --profile dev --no-deps --document-private-items
+# run `semgrep`
+semgrep:
+  env SEMGREP_ENABLE_VERSION_CHECK=0 \
+    semgrep --error --config .config/semgrep.yaml
 
-# fix some lint failures
-lint-fix:
-  just format
-  just clippy-fix
 
-# run all checks recommended before opening a PR
-final-check: lint
-  cargo test --doc
-  just test
-
+# check typos
 [no-exit-message]
 typos *PARAMS:
   #!/usr/bin/env bash
   set -eo pipefail
 
   git_ls_files="$(git ls-files)"
-  git_ls_nonbinary_files="$(echo "$git_ls_files" |  grep -v -E "^\.(png|gif)\$")"
+  git_ls_nonbinary_files="$(echo "$git_ls_files" |  grep -v -E "^db/|\.png\$|\.ods\$")"
 
-  if ! echo "$git_ls_nonbinary_files" | parallel typos {} ; then
+
+  if ! echo "$git_ls_nonbinary_files" | typos {{PARAMS}} --stdin-paths; then
     >&2 echo "Typos found: Valid new words can be added to '_typos.toml'"
     return 1
   fi
 
+# fix all typos
+[no-exit-message]
 typos-fix-all:
   just typos -w
-
-# run code formatters
-format:
-  cargo fmt --all
-  nixpkgs-fmt $(echo **.nix)
