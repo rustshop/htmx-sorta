f9f675f9e9bf001901fa830c513fda26846a3606
Dawid Ciężarkiewicz <dpc@dpc.pw>
2023-10-06 00:04:14 -0700

    chore: update Flakebox

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     flakebox = {
-      url = "github:rustshop/flakebox?rev=36b349dc4e6802a0a26bafa4baef1f39fbf4e870";
+      url = "github:rustshop/flakebox?rev=b07a9f3d17d400464210464e586f76223306f62d";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
@@ -16,41 +16,26 @@
         pkgs = import nixpkgs {
           inherit system;
         };
-        lib = pkgs.lib;
-        extLib = import ./nix/lib.nix { inherit lib; };
 
         flakeboxLib = flakebox.lib.${system} { };
         craneLib = flakeboxLib.craneLib;
 
-        commonArgs =
-          let
-            staticFilesFilter = path: type: if type == "directory" then lib.hasPrefix "/static/" path else lib.hasPrefix "/static/" path;
-          in
-          {
-            src = extLib.cleanSourceWithRel {
-              src = builtins.path {
-                name = "htmx-demo";
-                path = ./.;
-              };
-              filter = path: type:
-                (staticFilesFilter path type)
-                ||
-                (craneLib.filterCargoSources path type)
-              ;
-            };
-
-            installCargoArtifactsMode = "use-zstd";
-
-            buildInputs = [ ];
-
-            nativeBuildInputs = builtins.attrValues
-              {
-                inherit (pkgs) lld mold;
-              } ++ [ ];
+        src = flakeboxLib.filter.filterSubdirs {
+          root = builtins.path {
+            name = "htmx-demo";
+            path = ./.;
           };
+          dirs = [
+            "Cargo.toml"
+            "Cargo.lock"
+            ".cargo"
+            "src"
+            "static"
+          ];
+        };
       in
       {
-        packages.default = craneLib.buildPackage ({ } // commonArgs);
+        packages.default = craneLib.buildPackage { inherit src; };
 
         devShells = {
           default = flakeboxLib.mkDevShell {
